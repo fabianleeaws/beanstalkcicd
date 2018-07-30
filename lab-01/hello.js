8eec6dc35fb131f70674d47a09f2f8f180a0d4ae
Lee <leejin@c4b301d35313.ant.amazon.com>
2018-07-30 18:49:30 +0800

    clean up extras

--- lab-01/hello.js
+++ lab-01/hello.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const app = express()
-app.get('/', (req, res) => {
-    res.send('Hello world from a Node.js app!')
-})
-app.listen(3000, () => {
-    console.log('Server is up on 3000')
-})
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Hello world from a Node.js app!");
+});
+app.listen(80, () => {
+  console.log("Server is up on 3000");
+});
