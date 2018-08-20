d90bdd1d0b11890eef17a633b5f846f121510ba7
Lee <leejin@c4b301d35313.ant.amazon.com>
2018-08-20 19:47:54 +0800

    -m

--- lab-01/index.js
+++ lab-01/index.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const app = express();
-const appPort = 8080;
 
 app.get("/", (req, res) => {
   res.send("Hello world from a Node.js app!");
 });
-app.listen(appPort, () => {
-  console.log("Server is up on: " + appPort);
+app.listen(process.env.PORT, () => {
+  console.log("Server is up on: " + process.env.PORT);
 });
